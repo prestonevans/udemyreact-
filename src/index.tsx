e582533923c169a63f723479c6346c070fa7e907
Preston Evans <preston.evans3074@stu.mtec.edu>
2022-02-20 15:55:35 -0700

    react fragments and sematic html

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,25 @@
-import ReactDOM from "react-dom";
+import ReactDOM from 'react-dom';
 
 function Index() {
-    return(
-        <h1>Hello World</h1>
-    )
+	return (
+        <>
+            <header>
+                <h1>Hello World</h1>
+            </header>
+            <main>
+                <section>
+                    <h2>About me</h2>
+                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Minus voluptas laborum nostrum nam asperiores! Odio, mollitia id aspernatur sint saepe dignissimos perferendis officia reiciendis vitae amet, voluptas, alias aliquam ut laborum tempore. Enim mollitia dolores accusamus quod beatae vitae a esse doloribus quidem, sed amet magni. Asperiores praesentium necessitatibus numquam odit deserunt molestias minus non impedit pariatur nam! Mollitia expedita numquam facere dolore maiores quis, nam dignissimos inventore velit! Vel culpa sit atque molestiae praesentium delectus quam at exercitationem. Atque, aperiam debitis? Assumenda corporis quaerat suscipit est consequatur laboriosam aliquid omnis cum tempore incidunt, earum nostrum animi, odit repudiandae pariatur!</p>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis delectus ipsam distinctio magni quisquam totam nisi necessitatibus, aliquid iusto corrupti quam, amet nostrum nemo excepturi atque autem odit sunt. Molestias architecto facilis temporibus? Dolorum exercitationem molestias recusandae saepe, veniam ab!</p>
+                </section>
+                <section>
+                    <h2>Contact</h2>
+                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Minus voluptas laborum nostrum nam asperiores! Odio, mollitia id aspernatur sint saepe dignissimos perferendis officia reiciendis vitae amet, voluptas, alias aliquam ut laborum tempore. Enim mollitia dolores accusamus quod beatae vitae a esse doloribus quidem, sed amet magni. Asperiores praesentium necessitatibus numquam odit deserunt molestias minus non impedit pariatur nam! Mollitia expedita numquam facere dolore maiores quis, nam dignissimos inventore velit! Vel culpa sit atque molestiae praesentium delectus quam at exercitationem. Atque, aperiam debitis? Assumenda corporis quaerat suscipit est consequatur laboriosam aliquid omnis cum tempore incidunt, earum nostrum animi, odit repudiandae pariatur!</p>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis delectus ipsam distinctio magni quisquam totam nisi necessitatibus, aliquid iusto corrupti quam, amet nostrum nemo excepturi atque autem odit sunt. Molestias architecto facilis temporibus? Dolorum exercitationem molestias recusandae saepe, veniam ab!</p>
+                </section>
+            </main>
+        </>
+    );
 }
 
-ReactDOM.render(<Index />, document.querySelector('#root'))
+ReactDOM.render(<Index />, document.querySelector('#root'));
