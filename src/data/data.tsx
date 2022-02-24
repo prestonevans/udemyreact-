db2d11bfd99956b0636c1fb9f6d4cdbd7b084b0e
Preston Evans <preston.evans3074@stu.mtec.edu>
2022-02-23 21:36:27 -0700

    seperate code to make easier to read and alias booklist as test

---
+++ src/data/data.tsx
@@ -0,0 +1,42 @@
+export const bookList = [
+	{
+		img: 'https://images-na.ssl-images-amazon.com/images/I/91-EIJiYneL.jpg',
+		title: 'Atomic Habits',
+		author: 'James Clear',
+		rating: '3',
+		price: '3.99',
+		rank: '11'
+	},
+	{
+		img: 'https://images-na.ssl-images-amazon.com/images/I/91-EIJiYneL.jpg',
+		title: 'Atomic Something',
+		author: 'Preston Jay',
+		rating: '5',
+		price: '20',
+		rank: '15'
+	},
+	{
+		img: 'https://images-na.ssl-images-amazon.com/images/I/91-EIJiYneL.jpg',
+		title: 'Atomic Habits',
+		author: 'James Clear',
+		rating: '3',
+		price: '3.99',
+		rank: '11'
+	},
+	{
+		img: 'https://images-na.ssl-images-amazon.com/images/I/91-EIJiYneL.jpg',
+		title: 'Atomic Habits',
+		author: 'James Clear',
+		rating: '3',
+		price: '3.99',
+		rank: '11'
+	},
+	{
+		img: 'https://images-na.ssl-images-amazon.com/images/I/91-EIJiYneL.jpg',
+		title: 'Atomic Habits',
+		author: 'James Clear',
+		rating: '3',
+		price: '3.99',
+		rank: '11'
+	}
+];
--- src/index.tsx
+++ src/index.tsx
@@ -1,55 +1,13 @@
-import { useState } from 'react';
 import ReactDOM from 'react-dom';
 import { Book } from './components/books/books';
 import './index.css';
+import {bookList as test} from './data/data'
 
-const bookList = [
-	{
-		img: 'https://images-na.ssl-images-amazon.com/images/I/91-EIJiYneL.jpg',
-		title: 'Atomic Habits',
-		author: 'James Clear',
-		rating: '3',
-		price: '3.99',
-		rank: '11'
-	},
-	{
-		img: 'https://images-na.ssl-images-amazon.com/images/I/91-EIJiYneL.jpg',
-		title: 'Atomic Something',
-		author: 'Preston Jay',
-		rating: '5',
-		price: '20',
-		rank: '15'
-	},
-	{
-		img: 'https://images-na.ssl-images-amazon.com/images/I/91-EIJiYneL.jpg',
-		title: 'Atomic Habits',
-		author: 'James Clear',
-		rating: '3',
-		price: '3.99',
-		rank: '11'
-	},
-	{
-		img: 'https://images-na.ssl-images-amazon.com/images/I/91-EIJiYneL.jpg',
-		title: 'Atomic Habits',
-		author: 'James Clear',
-		rating: '3',
-		price: '3.99',
-		rank: '11'
-	},
-	{
-		img: 'https://images-na.ssl-images-amazon.com/images/I/91-EIJiYneL.jpg',
-		title: 'Atomic Habits',
-		author: 'James Clear',
-		rating: '3',
-		price: '3.99',
-		rank: '11'
-	}
-];
 
 function List() {
 	return (
 		<section>
-			{bookList.map((book, i) => {
+			{test.map((book, i) => {
 				return (
 					<Book {...book} key={`${book.rank}${i}`}/>
 				);
@@ -57,19 +15,5 @@ function List() {
 		</section>
 	);
 }
-
-// function List() {
-// 	const [tooltip, setTooltip] = useState(false)
-
-// 	function onFocusNameHandler() {
-// 		setTooltip(!tooltip)
-// 	}
-// 	return (
-// 		<main>
-// 			<span className={tooltip ? 'show' : 'hide'}>Here is a tooltip</span>
-// 			<input onBlur={onFocusNameHandler} onFocus={onFocusNameHandler} type="text" />
-// 		</main>
-// 	)
-// }
 
 ReactDOM.render(<List />, document.querySelector('#root'));
